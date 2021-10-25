09b06730b0223a6c2e0fbc897ff1fd8c672cd7ed
m1cha1s <msulej04@gmail.com>
2021-10-25 18:34:05 +0200

    Made some basic framework

---
+++ SHAL_core.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+class SHAL:
+    def __init__(self) -> None:
+
+        self.ia:int   = 0
+        self.ib:int   = 0
+
+        self.fa:float = 0
+        self.fb:float = 0
+
+        self.op:int = 0
---
+++ SHAL_mem.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+
+class MEM:
+    def __init__(self) -> None:
+        pass
